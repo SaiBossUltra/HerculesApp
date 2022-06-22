b17831a4f4d2de2f713dc8dd8ce1437c59aab247
Pvphantom <46140470+Pvphantom@users.noreply.github.com>
2022-06-21 18:01:01 -0700

    Screen for result added changed up search a little bit

--- app/src/main/java/com/example/herculesapp/homeScreen.kt
+++ app/src/main/java/com/example/herculesapp/homeScreen.kt
@@ -1,11 +1,20 @@
 package com.example.herculesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class homeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        val search: Button = findViewById (R.id.search)
+
+        search.setOnClickListener() {
+            val intent = Intent(this, homeScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
